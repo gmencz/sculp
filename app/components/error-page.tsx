402d7915cd700a5ccfef785f6c3cb59eb77b40ca
Gabriel <yo@gabrielmendezc.com>
2023-05-31 21:46:41 +0200

    history plcaeholder page

--- app/components/error-page.tsx
+++ app/components/error-page.tsx
@@ -14,7 +14,7 @@ export function ErrorPage({
   title,
 }: ErrorPageProps) {
   return (
-    <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+    <main className="grid min-h-screen place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
       <div className="text-center">
         <p className="text-base font-semibold text-orange-600">{statusCode}</p>
         <h1 className="mt-4 text-3xl font-bold tracking-tight text-zinc-900 sm:text-5xl">
--- app/routes/app.mesocycles._index.tsx
+++ app/routes/app.mesocycles._index.tsx
@@ -118,17 +118,17 @@ export default function Mesocycles() {
                   mesocycle.id === currentMesocycle.mesocycle.id ? (
                     <Link
                       to={`./${mesocycle.id}/stop`}
-                      className="relative -mr-px inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-bl-lg border border-transparent bg-red-100 py-4 text-sm font-semibold text-red-700"
+                      className="relative -mr-px inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-bl-lg border border-transparent bg-red-100 py-4 text-sm font-semibold text-red-700 hover:bg-red-200"
                     >
-                      Stop
+                      <span className="hidden xs:block">Stop</span>
                       <StopIcon className="h-5 w-5" aria-hidden="true" />
                     </Link>
                   ) : (
                     <Link
                       to={`./${mesocycle.id}/start`}
-                      className="relative -mr-px inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-bl-lg border border-transparent bg-orange-100 py-4 text-sm font-semibold text-orange-700"
+                      className="relative -mr-px inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-bl-lg border border-transparent bg-orange-100 py-4 text-sm font-semibold text-orange-700 hover:bg-orange-200"
                     >
-                      Start
+                      <span className="hidden xs:block">Start</span>
                       <PlayIcon className="h-5 w-5" aria-hidden="true" />
                     </Link>
                   )}
@@ -136,13 +136,28 @@ export default function Mesocycles() {
                 <div className="-ml-px flex w-0 flex-1">
                   <Link
                     to={`./${mesocycle.id}`}
-                    className="relative inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-br-lg border border-transparent py-4 text-sm font-semibold text-zinc-900"
+                    className="group relative inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-br-lg border border-transparent py-4 text-sm font-semibold text-zinc-900 hover:bg-zinc-100"
                   >
                     <PencilSquareIcon
-                      className="h-5 w-5 text-zinc-400"
+                      className="h-5 w-5 text-zinc-400 group-hover:text-zinc-700"
                       aria-hidden="true"
                     />
-                    Edit
+                    <span className="hidden xs:block">Edit</span>
+                  </Link>
+                </div>
+                <div className="-ml-px flex w-0 flex-1">
+                  <Link
+                    to={`./${mesocycle.id}/history`}
+                    className="group relative inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-br-lg border border-transparent py-4 text-sm font-semibold text-zinc-900 hover:bg-zinc-100"
+                  >
+                    <svg
+                      fill="currentColor"
+                      viewBox="0 0 24 24"
+                      className="h-5 w-5 text-zinc-400 group-hover:text-zinc-700"
+                    >
+                      <path d="M24 12c0 6.627-5.373 12-12 12s-12-5.373-12-12h2c0 5.514 4.486 10 10 10s10-4.486 10-10-4.486-10-10-10c-2.777 0-5.287 1.141-7.099 2.977l2.061 2.061-6.962 1.354 1.305-7.013 2.179 2.18c2.172-2.196 5.182-3.559 8.516-3.559 6.627 0 12 5.373 12 12zm-13-6v8h7v-2h-5v-6h-2z" />
+                    </svg>
+                    <span className="hidden xs:block">History</span>
                   </Link>
                 </div>
               </div>
