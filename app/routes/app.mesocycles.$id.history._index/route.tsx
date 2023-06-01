54d7de55d44f1157b8eef573f31d17da06610691
Gabriel <yo@gabrielmendezc.com>
2023-06-01 14:37:25 +0200

    improve ui

--- app/routes/app.mesocycles.$id.history._index/route.tsx
+++ app/routes/app.mesocycles.$id.history._index/route.tsx
@@ -1,3 +1,4 @@
+import { ArrowLongRightIcon } from "@heroicons/react/20/solid";
 import { Link, useLoaderData } from "@remix-run/react";
 import type { LoaderArgs } from "@remix-run/server-runtime";
 import { json } from "@remix-run/server-runtime";
@@ -31,50 +32,59 @@ export default function MesocycleHistory() {
   return (
     <AppPageLayout>
       <Heading>{mesocycle.name}</Heading>
-      <Paragraph>A history of the times you've done this mesocycle.</Paragraph>
+      <Paragraph>
+        {mesocycle.runs.length
+          ? `You've done this mesocycle ${mesocycle.runs.length} ${
+              mesocycle.runs.length === 1 ? "time" : "times"
+            }.`
+          : `You haven't done this mesocycle yet.`}
+      </Paragraph>
 
-      <table className="mt-4 min-w-full divide-y divide-zinc-300">
-        <thead>
-          <tr>
-            <th
-              scope="col"
-              className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-zinc-900 sm:pl-0"
-            >
-              Start date
-            </th>
-            <th
-              scope="col"
-              className="px-3 py-3.5 text-left text-sm font-semibold text-zinc-900"
-            >
-              Finish date
-            </th>
-            <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-0">
-              <span className="sr-only">Edit</span>
-            </th>
-          </tr>
-        </thead>
-        <tbody className="divide-y divide-zinc-200">
-          {mesocycle.runs.map((run) => (
-            <tr key={run.id}>
-              <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-zinc-900 sm:pl-0">
-                {format(new Date(run.startDate), "MMMM' 'd' 'yyyy")}
-              </td>
-              <td className="whitespace-nowrap px-3 py-4 text-sm text-zinc-500">
-                {format(new Date(run.endDate), "MMMM' 'd' 'yyyy")}
-              </td>
-
-              <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
-                <Link
-                  to={`./${run.id}`}
-                  className="text-orange-600 hover:text-orange-900"
-                >
-                  View<span className="sr-only">, {run.id}</span>
-                </Link>
-              </td>
+      {mesocycle.runs.length ? (
+        <table className="mt-4 min-w-full divide-y divide-zinc-300">
+          <thead>
+            <tr>
+              <th
+                scope="col"
+                className="py-3.5 pr-3 text-left text-sm font-semibold text-zinc-900 sm:pl-4"
+              >
+                Start date
+              </th>
+              <th
+                scope="col"
+                className="px-3 py-3.5 text-left text-sm font-semibold text-zinc-900"
+              >
+                Finish date
+              </th>
+              <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-0">
+                <span className="sr-only">Edit</span>
+              </th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody className="divide-y divide-zinc-200">
+            {mesocycle.runs.map((run) => (
+              <tr key={run.id}>
+                <td className="whitespace-nowrap py-4 pr-3 text-sm font-medium text-zinc-900 sm:pl-4">
+                  {format(new Date(run.startDate), "MMMM' 'd' 'yyyy")}
+                </td>
+                <td className="whitespace-nowrap px-3 py-4 text-sm text-zinc-500">
+                  {format(new Date(run.endDate), "MMMM' 'd' 'yyyy")}
+                </td>
+
+                <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
+                  <Link
+                    to={`./${run.id}`}
+                    className="flex items-center justify-center gap-2 text-orange-600 hover:text-orange-900"
+                  >
+                    <span className="hidden xs:inline">View training</span>
+                    <ArrowLongRightIcon className="h-6 w-6" />
+                  </Link>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      ) : null}
     </AppPageLayout>
   );
 }
