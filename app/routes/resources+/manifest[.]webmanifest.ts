45d98d90f275e4d63c2b95a2a4eb508b5a445884
Gabriel <yo@gabrielmendezc.com>
2023-06-07 19:45:11 +0200

    update workflow

--- app/routes/resources+/manifest[.]webmanifest.ts
+++ app/routes/resources+/manifest[.]webmanifest.ts
@@ -34,6 +34,18 @@ export const loader = async () => {
             },
           ],
         },
+        {
+          name: "Profile",
+          url: "/app/profile",
+          icons: [
+            {
+              src: "/icons/icon-96x96.png",
+              sizes: "96x96",
+              type: "image/png",
+              purpose: "any monochrome",
+            },
+          ],
+        },
       ],
       icons: [
         {
